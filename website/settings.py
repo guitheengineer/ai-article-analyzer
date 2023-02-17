93223f484dd9c111bd879a2fa3a42b5ec8d65941
guilhermefront <guilhermesnd14@gmail.com>
2023-02-17 14:21:43 -0300

    feat: add tailwind and initial logic with url

--- website/settings.py
+++ website/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'tailwind',
+    'theme',
+    'django_browser_reload'
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -122,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import requests
+from django.template import Context
 
 
 def index(request):
-    return render(request, 'website/index.html')
+    search = request.GET.get('search')
+    if search:
+        try:
+            # keywords = list()
+            # keywordCount = dict()
+            hasHttp = search.startswith(
+                'https://') or search.startswith('http://')
+            url = search
+
+            if not hasHttp:
+                url = 'http://' + url
+
+            page = urlopen(url)
+            html = page.read().decode('utf-8')
+            soup = BeautifulSoup(html, 'html.parser')
+            splittedWords = soup.get_text().split()
+            print(splittedWords)
+            for word in splittedWords:
+                normalizedWord = word.lower()
+                # if normalizedWord in keywords:
+                # keywordCount[normalizedWord] = keywordCount.get(
+                # normalizedWord, 0) + 1
+                # print(keywordCount)
+        except requests.ConnectionError:
+            print('Error: not able to connect to this website.')
+    return render(request, 'website/index.html', {'search': search})
