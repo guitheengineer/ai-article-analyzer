c8e440a2eece7ce46e5fb4b7c9fbfaa6a6c0c370
guilhermefront <guilhermesnd14@gmail.com>
2023-02-18 18:03:11 -0300

    feat: add AI positiviness detection

--- website/views.py
+++ website/views.py
@@ -1,17 +1,16 @@
-from django.http import HttpResponse
+from typing import Any
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
-from django.template import Context
+from transformers import pipeline
 
 
 def index(request):
     search = request.GET.get('search')
+    context = {'search': search or ''}
     if search:
         try:
-            # keywords = list()
-            # keywordCount = dict()
             hasHttp = search.startswith(
                 'https://') or search.startswith('http://')
             url = search
@@ -22,14 +21,12 @@ def index(request):
             page = urlopen(url)
             html = page.read().decode('utf-8')
             soup = BeautifulSoup(html, 'html.parser')
-            splittedWords = soup.get_text().split()
-            print(splittedWords)
-            for word in splittedWords:
-                normalizedWord = word.lower()
-                # if normalizedWord in keywords:
-                # keywordCount[normalizedWord] = keywordCount.get(
-                # normalizedWord, 0) + 1
-                # print(keywordCount)
+            articleText = ' '.join(soup.get_text().split())
+            print(articleText)
+            classifier = pipeline('sentiment-analysis')
+            positivity = classifier(articleText[:512])
+            context['positivity'] = positivity[0]['label']
+
         except requests.ConnectionError:
             print('Error: not able to connect to this website.')
-    return render(request, 'website/index.html', {'search': search})
+    return render(request, 'website/index.html', context)
