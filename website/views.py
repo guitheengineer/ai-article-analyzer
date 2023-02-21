b0da5089e422ebdc9b858abf5b47948fbc150502
guilhermefront <guilhermesnd14@gmail.com>
2023-02-21 12:30:43 -0300

    feat: add emotion to one of the measures

--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from trafilatura import fetch_url, extract
 from trafilatura.settings import use_config
-from transformers import pipeline
+from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
+
+tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
+model = TFRobertaForSequenceClassification.from_pretrained(
+    "arpanghoshal/EmoRoBERTa")
 
 # AI Model text limit
 text_limit = 512
@@ -17,18 +21,35 @@ def get_text_from_url(url: str) -> str | None:
     return article_text
 
 
-def measure_positivity(text: str):
+def get_positiviness(text: str):
+    positives = 0
+    total = 0
     classifier = pipeline(
         'sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')
-    label = classifier(text)[0]['label']  # type: ignore
-    return label
+    for i in range(0, len(text), text_limit):
+        text_chunk = text[i:i + text_limit]
+        label = classifier(text_chunk)[0]['label']  # type: ignore
+        total += 1
+        if label == 'POSITIVE':
+            positives += 1
+
+    positiveness = positives / total * 100
+    return positiveness
 
 
-def summarize(text: str):
+def get_summary(text: str):
     summarizer = pipeline(
         'summarization', model='philschmid/bart-large-cnn-samsum')
     result = summarizer(text)
     return result[0]['summary_text']  # type: ignore
+
+
+def get_emotion(text: str):
+
+    emotion_classifier = pipeline(
+        'sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
+    emotion = emotion_classifier(text)[0]['label']  # type: ignore
+    return emotion
 
 
 def index(request):
@@ -41,20 +62,12 @@ def index(request):
         if not text:
             error = 'There is no content to analyze.'
             return
-        positives = 0
-        total = 0
 
-        for i in range(0, len(text), text_limit):
-            text_chunk = text[i:i + text_limit]
-            result = measure_positivity(text_chunk)
-            total += 1
-            if result == 'POSITIVE':
-                positives += 1
-
-        positiveness = positives / total * 100
-
-        summary = summarize(text)
-        context['positiviness'] = positiveness
+        summary = get_summary(text)
+        positiviness = get_positiviness(text)
+        emotion = get_emotion(text)
         context['summary'] = summary
+        context['positiviness'] = positiviness
+        context['emotion'] = emotion
 
     return render(request, 'website/index.html', context)
